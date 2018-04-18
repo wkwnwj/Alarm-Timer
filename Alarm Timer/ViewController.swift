498669f1f9b079501e2b1d44d952acb94e2cafdb
csd5766 <csd5766@naver.com>
2018-04-18 12:10:51 +0900

    Lee

--- Alarm Timer/ViewController.swift
+++ Alarm Timer/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var alarmTime: UILabel!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        alarmTime.text = formatter.string(from: myDatePicker.date)
+        
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+            self.updateTime()
+        })
+    }
+    
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        currentTimeLabel.text = formatter.string(from: date)
+        
+        if currentTimeLabel.text == alarmTime.text {
+            view.backgroundColor = UIColor.red
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+
+    @IBAction func DatePicker(_ sender: Any) {
+        
+        print("change Date Picker")
+        print(myDatePicker.date)
+        
+        // dateFormat
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        alarmTime.text = formatter.string(from: myDatePicker.date)
     }
-
-
+    
+    @IBAction func stopAlert(_ sender: Any) {
+        view.backgroundColor = UIColor.red
+    }
+    
 }
 
